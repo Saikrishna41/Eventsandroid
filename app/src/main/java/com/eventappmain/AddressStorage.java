38077344b710fefe6cadbcbcccf455f112cd5f3e
Sai <saai.krrishna@gmail.com>
2015-12-21 17:37:26 +0530

    address stroage initial file

---
+++ app/src/main/java/com/eventappmain/AddressStorage.java
@@ -0,0 +1,12 @@
+package com.eventappmain;
+
+/**
+ * Created by Vicky on 03-Oct-15.
+ */
+public class AddressStorage {
+
+    public static String DisplayedTitle = null;
+    public static String DisplayedAddress = null;
+    public static boolean didUserSelect = false;
+
+}
